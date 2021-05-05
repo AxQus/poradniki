6151dd49eb8be7b69bcc2481924ae38ebf3e35db
AxQus <tsklyt@wp.pl>
2021-05-05 13:41:20 +0200

    update 2

--- Discord Bot/1 Install Bot/install.bat
+++ Discord Bot/1 Install Bot/install.bat
@@ -6,7 +6,12 @@ echo ------------------------------------
 echo 1. Zainstaluj nodejs z strony (https://nodejs.org/)
 Pause
 echo ------------------------------------
-echo 2. Zinstalowanie projektu (npm init) wypelnij wszystko
+echo 2. Instalowanie projektu (npm init) wypelnij wszystko
 echo npm init
 start npm init
+Pause
+echo ------------------------------------
+echo 3. Instalowanie discord.js (npm i discord.js)
+echo npm install discord.js
+start npm i discord.js
 Pause
