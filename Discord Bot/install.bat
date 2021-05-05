ce01c3730e098981fce7477e07ca9f4fcd2ab3b1
AxQus <tsklyt@wp.pl>
2021-05-05 13:18:21 +0200

    first commit

---
+++ Discord Bot/install.bat
@@ -0,0 +1,18 @@
+@echo off
+echo 
+echo ------------------------------------
+echo Poradnik jak zainstlowac bota v0.1 beta
+echo ------------------------------------
+echo 1. Zainstaluj nodejs z strony (https://nodejs.org/)
+Pause
+echo ------------------------------------
+echo 2. Zinstalowanie projektu (npm init) wypelnij wszystko
+echo npm init
+Pause
+echo ------------------------------------
+echo 3. stworz plik index.js
+echo wklej tutaj text >> index.js
+echo (https://nodejs.org/)
+echo ------------------------------------
+
+Pause
