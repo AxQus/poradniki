81c6ddb1f32e053c5c4fefee787c03bbebaf8a52
AxQus <tsklyt@wp.pl>
2021-05-05 13:43:26 +0200

    update 3

--- Discord Bot/1 Install Bot/index.js
+++ Discord Bot/1 Install Bot/index.js
@@ -2,16 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
+//Prefix bota - nie ma polecenia dodanego
 const prefix = (config.prefix);
 
+//Sygnalizuje że jest online
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Zalogowany ${client.user.tag}!`);
 });
 
+//Odpowiada na polecenie (ping)
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
 });
 
+//Loguje się do bota
 client.login(config.token);
